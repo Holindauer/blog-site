75ba202ce2343a7453147383bc8bca31bacfcdda
Holindauer <holindauer@gmail.com>
2023-11-04 18:14:40 -0700

    set up react proj basics

---
+++ src/pages/Blog_1.tsx
@@ -0,0 +1,16 @@
+// src/pages/BlogPost.tsx
+import React from 'react';
+
+const BlogPost = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>11/4/23 6:04 PM</h1>
+      <p>
+        Initial Blog Post
+      </p>
+      {/* Add more content and styling as needed */}
+    </div>
+  );
+};
+
+export default BlogPost;
